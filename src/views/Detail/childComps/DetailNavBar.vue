f3c762f5b81b4b92034f17276b16b4a2382ec617
StudyRecording <canmengduanqing@gmail.com>
2019-08-08 16:32:39 +0800

    详情页
    完善详情页的顶部栏和滚动图片

---
+++ src/views/Detail/childComps/DetailNavBar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <nav-bar>
+            <div slot="left" class="left" @click="leftClick">
+                <img src="~assets/img/common/back.svg" alt="">
+            </div>
+            <div slot="center" class="center">
+                <div v-for="(item, index) in titles" :key="index" 
+                class="center-item" :class="{active: index === currentIndex}"
+                @click="itemClick(index)">
+                    {{item}}
+                </div>
+            </div>
+        </nav-bar>
+    </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+export default {
+    name:'DetailNavBar',
+    components:{
+        NavBar
+    },
+    data() {
+        return {
+            titles:['商品', '参数 ', '评论', '推荐'],
+            currentIndex:0
+        }
+    },
+    methods:{
+        itemClick(index){
+            this.currentIndex = index;
+        },
+        leftClick() {
+            this.$router.back();
+        }
+    }
+}
+</script>
+
+<style scoped>
+.center {
+    display: flex;
+}
+.center-item {
+    flex: 1;
+}
+.active {
+    color: var(--color-high-text)
+}
+.left img {
+    margin-top: 10px;
+}
+</style>
+
+
--- src/views/Detail/detail.vue
+++ src/views/Detail/detail.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>{{iid}}</div>
+    <div id="detail">
+        <detail-nav-bar></detail-nav-bar>
+        <detail-swiper :topImages="topImages"></detail-swiper>
+    </div>
 </template>
 
 <script>
+import DetailNavBar from 'views/Detail/childComps/DetailNavBar'
+import DetailSwiper from 'views/Detail/childComps/DetailSwiper'
+
+import { getDetail } from 'network/detail'
 export default {
     name: 'Detail',
+    components:{
+        DetailNavBar,
+        DetailSwiper
+    },
     data() {
         return {
-            iid:null
+            iid:null,
+            topImages:[]
         }
     },
     created() {
         this.iid = this.$route.params.iid
+
+        getDetail(this.iid).then(res => {
+            console.log(res);
+            this.topImages = res.result.itemInfo.topImages
+        })
     }
 }
 </script>
